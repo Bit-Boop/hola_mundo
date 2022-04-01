e9b9197cc228b78238a8819570d48459777918e7
Taiel Espejo <taielesoejo40@gmail.com>
2022-04-01 17:48:01 -0300

    Initial commit

---
+++ src/archivos/hola_mundo.java
@@ -0,0 +1,11 @@
+package archivos;
+
+public class hola_mundo {
+    public static void main(String[] args) {
+        System.out.println("hola mundo puto");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
